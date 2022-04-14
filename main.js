22310665469e1556909d3dc076a114d2a33f8198
Eugen Dirksen <eugen@candlesofwisdom.com>
2022-04-14 10:05:58 +0200

    Add js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const cardAnswer = document.querySelector ('[data-js="cardAnswer"]');
-const buttonAnswer = document.querySelector ('[data-js="buttonAnswer"]');
+const cardAnswer = document.querySelector('[data-js="cardAnswer"]');
+const buttonAnswer = document.querySelector('[data-js="buttonAnswer"]');
 
-const bookmarkToggle = document.querySelector ('[data-js="bookmarkToggle"]');
+const bookmarkToggle = document.querySelector('[data-js="bookmarkToggle"]');
 
-buttonAnswer.addEventListener( "click", () => {
-    cardAnswer.classList.toggle("card__answer--visible")
-})
+buttonAnswer.addEventListener("click", () => {
+  cardAnswer.classList.toggle("card__answer--visible");
+});
 
-bookmarkToggle.addEventListener( "click", () => {
-    bookmarkToggle.classList.toggle("card__bookmark--set");
-})
+bookmarkToggle.addEventListener("click", () => {
+  bookmarkToggle.classList.toggle("card__bookmark--set");
+});
